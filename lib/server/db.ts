923785c8eff67dc960f46dcc79a0499cf8841d06
Divyansh_dev_ <divyanshnigam12345678@gmail.com>
2026-09-06 18:50:42 +0530

    Add type annotation to MONGODB_URI variable

--- lib/server/db.ts
+++ lib/server/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGO_URI;
+const MONGODB_URI: string = process.env.MONGO_URI ?? "";
 
 if (!MONGODB_URI) {
   throw new Error("MONGO_URI environment variable is required.");
